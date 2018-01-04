32fb73568b82ee36de77e9b068ea43876aa25d11
vermucht <vermucht@gmail.com>
2018-01-04 12:36:50 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**
+* Simple class to print message
+* 
+* @author Aleksei Sapozhnikov (vermucht@gmail.com)
+* @vesrion $Id$
+* @since 04.01.2018
+*/
 public class Calculate {
+	
+	/**
+	* Main method - program starts from here
+	* @param args - arguments
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
 	}
