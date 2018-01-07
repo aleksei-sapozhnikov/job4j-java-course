7d39e2b7715a2a16867b5d814df47e1b50e37375
vermucht <vermucht@gmail.com>
2018-01-07 10:13:53 +0300

    Fixed package-info information.

--- chapter_001/src/main/java/ru/job4j/condition/package-info.java
+++ chapter_001/src/main/java/ru/job4j/condition/package-info.java
@@ -1,4 +1,8 @@
 /**
  * Program showing distance between two points
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 07.01.2018
  */
 package ru.job4j.condition;
