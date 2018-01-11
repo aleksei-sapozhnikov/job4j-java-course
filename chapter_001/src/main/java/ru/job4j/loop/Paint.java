5449b06a82474cd8c4bc8a8a88a297b1954103f8
vermucht <vermucht@gmail.com>
2018-01-11 17:33:19 +0300

    4.5. Рефакторинг кода. [#33459]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,13 +1,66 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * Make strings in form of geometrical figures.
  *
  * @author Aleksei Sapozhnikov (vermucht@gmail.com)
  * @version $Id$
- * @since 09.01.2018
+ * @since 11.01.2018
  */
 public class Paint {
+
+    /**
+     * Returns Strings painted using ^ symbols, depending on given predicate.
+     *
+     * @param height    Height of the drawing (lines).
+     * @param width     Width of the drawing (columns).
+     * @param predicate Predicate.
+     * @return String with the drawing.
+     */
+    private String loopBy(int height, int width, BiPredicate<Integer, Integer> predicate) {
+        StringBuilder screen = new StringBuilder();
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != width; column++) {
+                if (predicate.test(row, column)) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
+    /**
+     * Paints right triangle.
+     *
+     * @param height Height (number of lines).
+     * @return String with right triangle.
+     */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+
+    /**
+     * Paints left triangle.
+     *
+     * @param height Height (number of lines).
+     * @return String with right triangle.
+     */
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
 
     /**
      * Draws pyramid using '^' symbols.
@@ -16,74 +69,11 @@ public class Paint {
      * @return String containing pyramid.
      */
     public String pyramid(int height) {
-        StringBuilder screen = new StringBuilder();
-        int weight = 2 * height - 1;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
     }
 
-    /**
-     * Paints right triangle.
-     *
-     * @param height Pyramid height (number of lines).
-     * @return String with right triangle.
-     */
-    public String rightTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        // width is equal to height
-        int width = height;
-        // rows (lines)
-        for (int row = 0; row != height; row++) {
-            // columns in line (cells)
-            for (int column = 0; column != width; column++) {
-                // printing ^ symbol where needed
-                if (row >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            // new line
-            screen.append(System.lineSeparator());
-        }
-        // result
-        return screen.toString();
-    }
-
-    /**
-     * Paints right triangle.
-     *
-     * @param height Pyramid height (number of lines).
-     * @return String with right triangle.
-     */
-    public String leftTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        // width is equal to height
-        int width = height;
-        // rows (lines)
-        for (int row = 0; row != height; row++) {
-            // columns in line (cells)
-            for (int column = 0; column != width; column++) {
-                // printing ^ symbol where needed
-                if (row >= width - column - 1) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            // new line
-            screen.append(System.lineSeparator());
-        }
-        // result
-        return screen.toString();
-    }
 }
