f6bbc74e5414a27a47a8526f1fd0c1d5d66df6b4
vermucht <vermucht@gmail.com>
2018-01-14 23:12:18 +0300

    К вопросу о сложности выяснения, содержится ли в строке другая строка.

--- chapter_001/src/main/java/ru/job4j/test/StringContains.java
+++ chapter_001/src/main/java/ru/job4j/test/StringContains.java
@@ -39,4 +39,34 @@ public class StringContains {
         return result;
     }
 
+    /**
+     * Checks if out string contains another string, using two for-loops.
+     *
+     * @param origin Original string.
+     * @param sub    String to check if is in original.
+     * @return true if contains, false if not.
+     */
+    public boolean containsUsingTwoForLoops(String origin, String sub) {
+        boolean result = false;
+        if (sub.length() <= origin.length()) {
+            char[] originArray = origin.toCharArray();
+            char[] subArray = sub.toCharArray();
+            for (int originIndex = 0; originIndex < originArray.length; originIndex++) {
+                for (int subIndex = 0; originArray[originIndex + subIndex] == subArray[subIndex]; subIndex++) {
+                    if (subIndex == subArray.length - 1) {
+                        result = true;
+                    }
+                    if (result) {
+                        break;
+                    }
+                }
+                if (result) {
+                    break;
+                }
+            }
+
+        }
+        return result;
+    }
+
 }
