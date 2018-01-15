2d9e5ce613d7ed219869297e563a4767b599cc2d
vermucht <vermucht@gmail.com>
2018-01-15 11:26:44 +0300

    Проверка, что одно слово находится в другом слове [#228]

--- chapter_001/src/main/java/ru/job4j/test/StringContains.java
+++ chapter_001/src/main/java/ru/job4j/test/StringContains.java
@@ -21,37 +21,7 @@ public class StringContains {
         if (sub.length() <= origin.length()) {
             char[] originArray = origin.toCharArray();
             char[] subArray = sub.toCharArray();
-            int subIndex = 0;
-            for (char originChar : originArray) {
-                if (result) {
-                    break;
-                }
-                if (originChar == subArray[subIndex]) {
-                    subIndex++;
-                } else {
-                    subIndex = 0;
-                }
-                if (subIndex == subArray.length) {
-                    result = true;
-                }
-            }
-        }
-        return result;
-    }
-
-    /**
-     * Checks if out string contains another string, using two for-loops.
-     *
-     * @param origin Original string.
-     * @param sub    String to check if is in original.
-     * @return true if contains, false if not.
-     */
-    public boolean containsUsingTwoForLoops(String origin, String sub) {
-        boolean result = false;
-        if (sub.length() <= origin.length()) {
-            char[] originArray = origin.toCharArray();
-            char[] subArray = sub.toCharArray();
-            for (int originIndex = 0; originIndex < originArray.length; originIndex++) {
+            for (int originIndex = 0; originIndex + subArray.length - 1 < originArray.length; originIndex++) {
                 for (int subIndex = 0; originArray[originIndex + subIndex] == subArray[subIndex]; subIndex++) {
                     if (subIndex == subArray.length - 1) {
                         result = true;
