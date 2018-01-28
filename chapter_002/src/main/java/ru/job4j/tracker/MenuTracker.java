e0e788424ac2a9a9bff6fd5997f8f89ed5f0fb01
vermucht <vermucht@gmail.com>
2018-01-29 00:38:22 +0300

    1. Реализовать события на внутренних классах. [#787]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
@@ -0,0 +1,431 @@
+package ru.job4j.tracker;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Shows menu, launches user actions.
+ */
+public class MenuTracker {
+
+    /**
+     * Where the user's requests are taken from.
+     */
+    private Input input;
+
+    /**
+     * Where to store information about items.
+     */
+    private Tracker tracker;
+
+    /**
+     * All possible user actions.
+     */
+    private UserAction[] userActions = new UserAction[7];
+
+
+    /**
+     * Constructor.
+     *
+     * @param input   Where to take user input from.
+     * @param tracker Where to store information.
+     */
+    public MenuTracker(Input input, Tracker tracker) {
+        this.input = input;
+        this.tracker = tracker;
+    }
+
+    /**
+     * Fill array with possible actions.
+     */
+    public void fillUserActions() {
+        this.userActions[0] = this.new AddItem();
+        this.userActions[1] = new MenuTracker.ShowAllItems();
+        this.userActions[2] = new EditItem();
+        this.userActions[3] = this.new DeleteItem();
+        this.userActions[4] = new MenuTracker.FindItemById();
+        this.userActions[5] = new FindItemsByName();
+        this.userActions[6] = new Exit();
+    }
+
+    public void show() {
+        System.out.println();
+        System.out.println("============ Action menu ============");
+        for (UserAction action : this.userActions) {
+            if (action != null) {
+                System.out.println(action.menuLine());
+            }
+        }
+    }
+
+    /**
+     * Launches action chosen by key.
+     *
+     * @param key Action key.
+     */
+    public void launchAction(String key) {
+        this.userActions[Integer.valueOf(key)].execute(this.input, this.tracker);
+    }
+
+    /**
+     * <<<<<<< HEAD
+     * Program initialization.
+     */
+    public void init() {
+        //
+    }
+
+
+    /**
+     * =======
+     * >>>>>>> Реализовать события на внутренних классах. [#787]
+     * Action : add item.
+     */
+    private class AddItem implements UserAction {
+
+        /**
+         * Key of this action.
+         */
+        private static final int KEY = 0;
+
+        /**
+         * <<<<<<< HEAD
+         * Description of the action
+         * =======
+         * Description of the action.
+         * >>>>>>> Реализовать события на внутренних классах. [#787]
+         */
+        private static final String DESCRIPTION = "Add new Item";
+
+        /**
+         * Get key of the action.
+         *
+         * @return Value of the KEY constant.
+         */
+        public int getActionKey() {
+            return KEY;
+        }
+
+        /**
+         * Performs operations made by this this action.
+         *
+         * @param input   Where to take user input from.
+         * @param tracker Where to store items.
+         */
+        public void execute(Input input, Tracker tracker) {
+            System.out.println();
+            System.out.println("------------ Add new item ------------");
+            String name = input.ask("Enter item name : ");
+            String desc = input.ask("Enter item description : ");
+            Item item = tracker.add(new Item(name, desc, System.currentTimeMillis()));
+            System.out.println(String.format("=== New item added. Item id : %s", item.getId()));
+
+        }
+
+        public String menuLine() {
+            return String.format("%s : %s", KEY, DESCRIPTION);
+        }
+    }
+
+    /**
+     * <<<<<<< HEAD
+     * =======
+     * Action : delete item.
+     */
+    private class DeleteItem implements UserAction {
+
+        /**
+         * Key of this action.
+         */
+        private static final int KEY = 3;
+
+        /**
+         * Description of the action.
+         */
+        private static final String DESCRIPTION = "Delete item";
+
+        /**
+         * Get key of the action.
+         *
+         * @return Value of the KEY constant.
+         */
+        public int getActionKey() {
+            return KEY;
+        }
+
+        /**
+         * Performs operations made by this this action.
+         *
+         * @param input   Where to take user input from.
+         * @param tracker Where to store items.
+         */
+        public void execute(Input input, Tracker tracker) {
+            System.out.println();
+            System.out.println("------------ Delete item ------------");
+            String delId = input.ask("Enter item id : ");
+            //confirm
+            if ("y".equals(input.ask("Confirm delete (y to continue)? : "))) {
+                tracker.delete(delId);
+                System.out.println("=== Item deleted.");
+            } else {
+                System.out.println("=== Operation aborted.");
+            }
+        }
+
+        public String menuLine() {
+            return String.format("%s : %s", KEY, DESCRIPTION);
+        }
+    }
+
+    /**
+     * >>>>>>> Реализовать события на внутренних классах. [#787]
+     * Action : show all items contained.
+     */
+    private static class ShowAllItems implements UserAction {
+
+        /**
+         * Key of this action.
+         */
+        private static final int KEY = 1;
+
+        /**
+         * <<<<<<< HEAD
+         * Description of the action
+         * =======
+         * Description of the action.
+         * >>>>>>> Реализовать события на внутренних классах. [#787]
+         */
+        private static final String DESCRIPTION = "Show all items";
+
+        /**
+         * Get key of the action.
+         *
+         * @return Value of the KEY constant.
+         */
+        public int getActionKey() {
+            return KEY;
+        }
+
+        /**
+         * Performs operations made by this this action.
+         *
+         * @param input   Where to take user input from.
+         * @param tracker Where to store items.
+         */
+        public void execute(Input input, Tracker tracker) {
+            System.out.println();
+            System.out.println("------------ Show all items contained ------------");
+            Item[] items = tracker.findAll();
+            for (Item item : items) {
+                System.out.println();
+                System.out.println(String.format("=== Item id : %s", item.getId()));
+                System.out.println(String.format("name : %s", item.getName()));
+                System.out.println(String.format("description : %s", item.getDescription()));
+                System.out.println(String.format("created : %s", new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(new Date(item.getCreateTime()))));
+            }
+        }
+
+        public String menuLine() {
+            return String.format("%s : %s", KEY, DESCRIPTION);
+        }
+    }
+
+    /**
+     * Action : find item by id.
+     */
+    private static class FindItemById implements UserAction {
+
+        /**
+         * Key of this action.
+         */
+        private static final int KEY = 4;
+
+        /**
+         * Description of the action.
+         */
+        private static final String DESCRIPTION = "Find item by Id";
+
+        /**
+         * Get key of the action.
+         *
+         * @return Value of the KEY constant.
+         */
+        public int getActionKey() {
+            return KEY;
+        }
+
+        /**
+         * Performs operations made by this this action.
+         *
+         * @param input   Where to take user input from.
+         * @param tracker Where to store items.
+         */
+        public void execute(Input input, Tracker tracker) {
+            System.out.println();
+            System.out.println("------------ Find item by id ------------");
+            String findId = input.ask("Enter item id : ");
+            Item item = tracker.findById(findId);
+            System.out.println("=== Item information : ");
+            System.out.println(String.format("id : %s", item.getId()));
+            System.out.println(String.format("name : %s", item.getName()));
+            System.out.println(String.format("description : %s", item.getDescription()));
+            System.out.println(String.format("created : %s", new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(new Date(item.getCreateTime()))));
+        }
+
+        public String menuLine() {
+            return String.format("%s : %s", KEY, DESCRIPTION);
+        }
+    }
+}
+
+/**
+ * Action : edit item information.
+ */
+class EditItem implements UserAction {
+
+    /**
+     * Key of this action.
+     */
+    private static final int KEY = 2;
+
+    /**
+     * Description of the action
+     */
+    private static final String DESCRIPTION = "Edit item";
+
+    /**
+     * Get key of the action.
+     *
+     * @return Value of the KEY constant.
+     */
+    public int getActionKey() {
+        return KEY;
+    }
+
+    /**
+     * Performs operations made by this this action.
+     *
+     * @param input   Where to take user input from.
+     * @param tracker Where to store items.
+     */
+    public void execute(Input input, Tracker tracker) {
+        System.out.println();
+        System.out.println("------------ Edit item ------------");
+        String editId = input.ask("Enter item id : ");
+        Item oldItem = tracker.findById(editId);
+        //show item data
+        System.out.println(String.format("id : %s", oldItem.getId()));
+        System.out.println(String.format("name : %s", oldItem.getName()));
+        System.out.println(String.format("description : %s", oldItem.getDescription()));
+        System.out.println(String.format("created : %s", new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(new Date(oldItem.getCreateTime()))));
+        //get new data
+        System.out.println();
+        String name = input.ask("Enter item new name : ");
+        String desc = input.ask("Enter item new description : ");
+        //store old information
+        Item newItem = new Item(name, desc, oldItem.getCreateTime());
+        newItem.setId(oldItem.getId());
+        //confirm
+        if ("y".equals(input.ask("Confirm change (y to continue)? : "))) {
+            tracker.replace(editId, newItem);
+            System.out.println("=== Item information updated.");
+        } else {
+            System.out.println("=== Operation aborted.");
+        }
+    }
+
+    public String menuLine() {
+        return String.format("%s : %s", KEY, DESCRIPTION);
+    }
+
+}
+
+/**
+ * Action : find items by given name.
+ */
+class FindItemsByName implements UserAction {
+
+    /**
+     * Key of this action.
+     */
+    private static final int KEY = 5;
+
+    /**
+     * Description of the action
+     */
+    private static final String DESCRIPTION = "Find items by name";
+
+    /**
+     * Get key of the action.
+     *
+     * @return Value of the KEY constant.
+     */
+    public int getActionKey() {
+        return KEY;
+    }
+
+    /**
+     * Performs operations made by this this action.
+     *
+     * @param input   Where to take user input from.
+     * @param tracker Where to store items.
+     */
+    public void execute(Input input, Tracker tracker) {
+        System.out.println();
+        System.out.println("------------ Find items with given name ------------");
+        String findName = input.ask("Enter name : ");
+        Item[] items = tracker.findByName(findName);
+        for (Item item : items) {
+            System.out.println();
+            System.out.println(String.format("== Item id : %s", item.getId()));
+            System.out.println(String.format("name : %s", item.getName()));
+            System.out.println(String.format("description : %s", item.getDescription()));
+            System.out.println(String.format("created : %s", new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(new Date(item.getCreateTime()))));
+        }
+    }
+
+    public String menuLine() {
+        return String.format("%s : %s", KEY, DESCRIPTION);
+    }
+
+}
+
+/**
+ * Action : exit from program.
+ */
+class Exit implements UserAction {
+
+    /**
+     * Key of this action.
+     */
+    private static final int KEY = 6;
+
+    /**
+     * Description of the action
+     */
+    private static final String DESCRIPTION = "Exit Program";
+
+    /**
+     * Get key of the action.
+     *
+     * @return Value of the KEY constant.
+     */
+    public int getActionKey() {
+        return KEY;
+    }
+
+    /**
+     * Performs operations made by this this action.
+     *
+     * @param input   Where to take user input from.
+     * @param tracker Where to store items.
+     */
+    public void execute(Input input, Tracker tracker) {
+        System.out.println("=== Exit program.");
+    }
+
+    public String menuLine() {
+        return String.format("%s : %s", KEY, DESCRIPTION);
+    }
+
+}
--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,8 +1,5 @@
 package ru.job4j.tracker;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 /**
  * Object to store in tracker.
  *
@@ -13,40 +10,9 @@ import java.util.Date;
 public class StartUI {
 
     /**
-     * Menu constant to add new item.
-     */
-    private static final String ADD = "0";
-
-    /**
-     * Menu constant to show all items in storage.
-     */
-    private static final String SHOW_ALL = "1";
-
-    /**
-     * Menu constant to edit item.
-     */
-    private static final String EDIT = "2";
-
-    /**
-     * Menu constant to delete item.
-     */
-    private static final String DELETE = "3";
-
-    /**
-     * Menu constant to find item by id.
-     */
-    private static final String FIND_BY_ID = "4";
-
-    /**
-     * Menu constant to find items by name.
-     */
-    private static final String FIND_BY_NAME = "5";
-
-    /**
      * Menu constant to exit from program.
      */
     private static final String EXIT = "6";
-
 
     /**
      * Where from input is taken.
@@ -58,7 +24,6 @@ public class StartUI {
      */
     private final Tracker tracker;
 
-
     /**
      * Constructor
      *
@@ -69,7 +34,6 @@ public class StartUI {
         this.input = input;
         this.tracker = tracker;
     }
-
 
     /**
      * Main method, starts the program.
@@ -85,146 +49,15 @@ public class StartUI {
      */
     public void init() {
         boolean exit = false;
+        MenuTracker menu = new MenuTracker(this.input, this.tracker);
+        menu.fillUserActions();
         while (!exit) {
-            this.showMenu();
+            menu.show();
             String answer = this.input.ask("Enter number for action : ");
-            if (ADD.equals(answer)) {
-                this.addItem();
-            } else if (SHOW_ALL.equals(answer)) {
-                this.showAllItems();
-            } else if (EDIT.equals(answer)) {
-                this.editItem();
-            } else if (DELETE.equals(answer)) {
-                this.deleteItem();
-            } else if (FIND_BY_ID.equals(answer)) {
-                this.findByIdItem();
-            } else if (FIND_BY_NAME.equals(answer)) {
-                this.findByNameItem();
-            } else if (EXIT.equals(answer)) {
+            menu.launchAction(answer);
+            if (EXIT.equals(answer)) {
                 exit = true;
-                System.out.println("=== Exit program.");
-            } else {
-                System.out.println("=== Unknown operation.");
             }
-        }
-    }
-
-    /**
-     * Display menu with action variants.
-     */
-    private void showMenu() {
-        System.out.println();
-        System.out.println("============ Action menu ============");
-        System.out.println(String.format("%s : Add new Item", ADD));
-        System.out.println(String.format("%s : Show all items", SHOW_ALL));
-        System.out.println(String.format("%s : Edit item", EDIT));
-        System.out.println(String.format("%s : Delete item", DELETE));
-        System.out.println(String.format("%s : Find item by Id", FIND_BY_ID));
-        System.out.println(String.format("%s : Find items by name", FIND_BY_NAME));
-        System.out.println(String.format("%s : Exit Program", EXIT));
-    }
-
-    /**
-     * Add new item to storage.
-     */
-    private void addItem() {
-        System.out.println();
-        System.out.println("------------ Add new item ------------");
-        String name = this.input.ask("Enter item name : ");
-        String desc = this.input.ask("Enter item description : ");
-        Item item = this.tracker.add(new Item(name, desc, System.currentTimeMillis()));
-        System.out.println(String.format("=== New item added. Item id : %s", item.getId()));
-    }
-
-    /**
-     * Show all items contained.
-     */
-    private void showAllItems() {
-        System.out.println();
-        System.out.println("------------ Show all items contained ------------");
-        Item[] items = this.tracker.findAll();
-        for (Item item : items) {
-            System.out.println();
-            System.out.println(String.format("=== Item id : %s", item.getId()));
-            System.out.println(String.format("name : %s", item.getName()));
-            System.out.println(String.format("description : %s", item.getDescription()));
-            System.out.println(String.format("created : %s", new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(new Date(item.getCreateTime()))));
-        }
-    }
-
-    /**
-     * Edit item.
-     */
-    private void editItem() {
-        System.out.println();
-        System.out.println("------------ Edit item ------------");
-        String editId = this.input.ask("Enter item id : ");
-        Item oldItem = this.tracker.findById(editId);
-        //show item data
-        System.out.println(String.format("id : %s", oldItem.getId()));
-        System.out.println(String.format("name : %s", oldItem.getName()));
-        System.out.println(String.format("description : %s", oldItem.getDescription()));
-        System.out.println(String.format("created : %s", new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(new Date(oldItem.getCreateTime()))));
-        //get new data
-        System.out.println();
-        String name = this.input.ask("Enter item new name : ");
-        String desc = this.input.ask("Enter item new description : ");
-        Item newItem = new Item(name, desc, oldItem.getCreateTime());
-        newItem.setId(oldItem.getId());
-        //confirm
-        if ("y".equals(input.ask("Confirm change (y to continue)? : "))) {
-            this.tracker.replace(editId, newItem);
-            System.out.println("=== Item information updated.");
-        } else {
-            System.out.println("=== Operation aborted.");
-        }
-    }
-
-    /**
-     * Delete item.
-     */
-    private void deleteItem() {
-        System.out.println();
-        System.out.println("------------ Delete item ------------");
-        String delId = this.input.ask("Enter item id : ");
-        //confirm
-        if ("y".equals(input.ask("Confirm delete (y to continue)? : "))) {
-            this.tracker.delete(delId);
-            System.out.println("=== Item deleted.");
-        } else {
-            System.out.println("=== Operation aborted.");
-        }
-    }
-
-    /**
-     * Find item by id.
-     */
-    private void findByIdItem() {
-        System.out.println();
-        System.out.println("------------ Find item by id ------------");
-        String findId = this.input.ask("Enter item id : ");
-        Item item = this.tracker.findById(findId);
-        System.out.println("=== Item information : ");
-        System.out.println(String.format("id : %s", item.getId()));
-        System.out.println(String.format("name : %s", item.getName()));
-        System.out.println(String.format("description : %s", item.getDescription()));
-        System.out.println(String.format("created : %s", new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(new Date(item.getCreateTime()))));
-    }
-
-    /**
-     * Find items with given name.
-     */
-    private void findByNameItem() {
-        System.out.println();
-        System.out.println("------------ Find items with given name ------------");
-        String findName = this.input.ask("Enter name : ");
-        Item[] items = tracker.findByName(findName);
-        for (Item item : items) {
-            System.out.println();
-            System.out.println(String.format("== Item id : %s", item.getId()));
-            System.out.println(String.format("name : %s", item.getName()));
-            System.out.println(String.format("description : %s", item.getDescription()));
-            System.out.println(String.format("created : %s", new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(new Date(item.getCreateTime()))));
         }
     }
 
