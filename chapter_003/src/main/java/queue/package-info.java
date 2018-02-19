e334fcf4304d25691afa0be99ed98683940fe89b
vermucht <vermucht@gmail.com>
2018-02-19 17:46:17 +0300

    2. Очередь с приоритетом на LinkedList [#41670]

--- chapter_003/src/main/java/queue/package-info.java
+++ chapter_003/src/main/java/queue/package-info.java
@@ -1,8 +1,8 @@
 /**
- * Simple phone book.
+ * Sinmple priority queue.
  *
  * @author Aleksei Sapozhnikov (vermucht@gmail.com)
  * @version $Id$
  * @since 19.02.2018
  */
-package phonebook;
+package queue;
---
+++ chapter_003/src/test/java/queue/TaskTest.java
@@ -0,0 +1,29 @@
+package queue;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TaskTest {
+
+    /**
+     * Test description() method.
+     */
+    @Test
+    public void whenDescriptionThenDescriptionString() {
+        Task task = new Task("desc", 4);
+        String result = task.description();
+        assertThat(result, is("desc"));
+    }
+
+    /**
+     * Test priority() method.
+     */
+    @Test
+    public void whenPriorityThenPriorityString() {
+        Task task = new Task("desc", 4);
+        int result = task.priority();
+        assertThat(result, is(4));
+    }
+}
