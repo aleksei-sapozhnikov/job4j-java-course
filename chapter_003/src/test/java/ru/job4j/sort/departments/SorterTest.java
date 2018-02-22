c686793c4dfdef1262532902630211b6462099b2
vermucht <vermucht@gmail.com>
2018-02-23 00:23:47 +0300

    Отсортировать департаменты [#34352]

---
+++ chapter_003/src/test/java/ru/job4j/sort/departments/SorterTest.java
@@ -0,0 +1,73 @@
+package ru.job4j.sort.departments;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Tests for the HeadNodes class.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 24.02.2018
+ */
+public class SorterTest {
+
+    /**
+     * Test parseArray() method.
+     */
+    @Test
+    public void whenArrayThenNewArrayNaturalOrder() {
+        String[] input = {
+                "K1\\SK1\\SSK2",
+                "K1\\SK2",
+                "K1\\SK1\\SSK1",
+                "K2\\SK1\\SSK2",
+                "K1\\SK1",
+                "K2\\SK1\\SSK1",
+                "K2",
+        };
+        String[] result = new Sorter().sortArray(input);
+        String[] expected = {
+                "K1",
+                "K1\\SK1",
+                "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2",
+                "K1\\SK2",
+                "K2",
+                "K2\\SK1",
+                "K2\\SK1\\SSK1",
+                "K2\\SK1\\SSK2"
+        };
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenArrayThenNewArrayReverseOrder() {
+        String[] input = {
+                "K1\\SK1\\SSK2",
+                "K1\\SK2",
+                "K1\\SK1\\SSK1",
+                "K2\\SK1\\SSK2",
+                "K1\\SK1",
+                "K2\\SK1\\SSK1",
+                "K2",
+        };
+        String[] result = new Sorter(Comparator.reverseOrder()).sortArray(input);
+        String[] expected = {
+                "K2",
+                "K2\\SK1",
+                "K2\\SK1\\SSK2",
+                "K2\\SK1\\SSK1",
+                "K1",
+                "K1\\SK2",
+                "K1\\SK1",
+                "K1\\SK1\\SSK2",
+                "K1\\SK1\\SSK1",
+        };
+        assertThat(result, is(expected));
+    }
+}
