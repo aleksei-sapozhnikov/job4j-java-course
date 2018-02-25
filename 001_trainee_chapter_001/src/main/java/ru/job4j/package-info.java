562a52a2e8e590bef01ebe8c52ad1e143e461ad5
vermucht <vermucht@gmail.com>
2018-02-25 21:06:23 +0300

    Переименовал модули, чтобы продолжить структуру.

---
+++ 001_trainee_chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for studying.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 04.01.2018
+ */
+package ru.job4j;
---
+++ 001_trainee_chapter_001/src/test/java/ru/job4j/loops/FactorialTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.loops;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Tests for Factorial class.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 09.01.2018
+ */
+public class FactorialTest {
+
+    /**
+     * Test calc method.
+     */
+    @Test
+    public void whenZeroThenOne() {
+        Factorial fact = new Factorial();
+        int result = fact.calc(0);
+        int expected = 1;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenOneThenOne() {
+        Factorial fact = new Factorial();
+        int result = fact.calc(1);
+        int expected = 1;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenTwoThenTwo() {
+        Factorial fact = new Factorial();
+        int result = fact.calc(2);
+        int expected = 2;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenThreeThenSix() {
+        Factorial fact = new Factorial();
+        int result = fact.calc(3);
+        int expected = 6;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenFiveThenOneHundredAndTwenty() {
+        Factorial fact = new Factorial();
+        int result = fact.calc(5);
+        int expected = 120;
+        assertThat(result, is(expected));
+    }
+    
+}
