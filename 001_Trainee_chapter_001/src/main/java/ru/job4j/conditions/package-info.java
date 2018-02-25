27cd0a5bb569a5b4bdaefc46fe6dc8db5d19badd
vermucht <vermucht@gmail.com>
2018-02-25 21:02:16 +0300

    Переименовал модули, чтобы продолжить структуру.

--- 001_Trainee_chapter_001/src/main/java/ru/job4j/conditions/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Simple programs to study conditions in Java.
- *
- * @author Aleksei Sapozhnikov (vermucht@gmail.com)
- * @version $Id$
- * @since 07.01.2018
- */
-package ru.job4j.conditions;
--- 001_Trainee_chapter_001/src/main/java/ru/job4j/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Package for studying.
- *
- * @author Aleksei Sapozhnikov (vermucht@gmail.com)
- * @version $Id$
- * @since 04.01.2018
- */
-package ru.job4j;
