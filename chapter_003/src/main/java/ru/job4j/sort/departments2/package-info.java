bc06edf143df3941805203e4566baa8267d7f44e
vermucht <vermucht@gmail.com>
2018-02-25 17:56:05 +0300

    Отсортировать департаменты [#34352] - 2й вариант

---
+++ chapter_003/src/main/java/ru/job4j/sort/departments2/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Sort departments saving their hierarchy.
+ * Here - using comparing of strings only, without tree.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 25.02.2018
+ */
+package ru.job4j.sort.departments2;
