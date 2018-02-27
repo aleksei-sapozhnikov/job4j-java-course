d330328a91fcd10830c540056b054a2e3bd560a5
vermucht <vermucht@gmail.com>
2018-02-27 12:07:44 +0300

    5.1.2. Создать итератор четные числа [#150]

---
+++ 002_junior_chapter_001/src/main/java/iterator/even/IteratorEven.java
@@ -0,0 +1,68 @@
+package iterator.even;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Array iterator, returning only even numbers.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 26.02.2018
+ */
+public class IteratorEven implements Iterator<Integer> {
+
+    /**
+     * Current cursor outer array position(row of matrix).
+     */
+    private int cursor = 0;
+
+    /**
+     * Array to iterate.
+     */
+    private final int[] values;
+
+    IteratorEven(final int[] values) {
+        this.values = values;
+        this.moveCursorToEven();
+    }
+
+    /**
+     * Returns {@code true} if the iteration has more elements.
+     * (In other words, returns {@code true} if {@link #next} would
+     * return an element rather than throwing an exception.)
+     *
+     * @return {@code true} if the iteration has more elements
+     */
+    @Override
+    public boolean hasNext() {
+        return this.cursor < this.values.length;
+    }
+
+    /**
+     * Returns the next element in the iteration.
+     *
+     * @return the next element in the iteration
+     * @throws NoSuchElementException if the iteration has no more elements
+     */
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        Integer result = this.values[this.cursor];
+        cursor++;
+        this.moveCursorToEven();
+        return result;
+    }
+
+    /**
+     * Move cursor to the next even number element.
+     */
+    private void moveCursorToEven() {
+        while (this.cursor < this.values.length && this.values[this.cursor] % 2 != 0) {
+            this.cursor++;
+        }
+    }
+
+}
---
+++ 002_junior_chapter_001/src/main/java/iterator/even/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Array iterator, returning only even numbers.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 26.02.2018
+ */
+package iterator.even;
