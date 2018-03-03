898b2b9e1d32a9bf5673a4c2dfac55cb1904f4b8
vermucht <vermucht@gmail.com>
2018-03-03 18:01:19 +0300

    5.3.3. Используя контейнер на базе связанного списка создать контейнер Stack и Queue. [#160]

--- 002_junior_chapter_001/src/test/java/ru/job4j/list/stack/SimpleStackTest.java
+++ 002_junior_chapter_001/src/test/java/ru/job4j/list/stack/SimpleStackTest.java
@@ -25,19 +25,19 @@ public class SimpleStackTest {
     @Test
     public void whenPushAndPollInDifferentOrderThatDoesntAffectOnResult() {
         SimpleStack<String> stack = new SimpleStack<>();
-        stack.push("1");
+        stack.push("1"); // 1
         assertThat(stack.poll(), is("1"));
         stack.push("2");
-        stack.push("3");
+        stack.push("3"); // 2 3
         assertThat(stack.poll(), is("3"));
-        stack.push("4");
+        stack.push("4"); // 2 4
         assertThat(stack.poll(), is("4"));
         assertThat(stack.poll(), is("2"));
         assertThat(stack.poll(), is((String) null));
         stack.push("5");
-        stack.push("6");
+        stack.push("6"); // 5 6
         assertThat(stack.poll(), is("6"));
-        stack.push("7");
+        stack.push("7"); // 5 7
         assertThat(stack.poll(), is("7"));
         assertThat(stack.poll(), is("5"));
         assertThat(stack.poll(), is((String) null));
