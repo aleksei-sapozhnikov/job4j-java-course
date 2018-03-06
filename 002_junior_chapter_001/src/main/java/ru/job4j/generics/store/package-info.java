4b8709436218d05888512f0c6ae31244e3bd7548
vermucht <vermucht@gmail.com>
2018-03-06 10:19:45 +0300

    Javadoc

---
+++ 002_junior_chapter_001/src/main/java/ru/job4j/generics/store/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Simple storage based on list, for studying generics.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 01.03.2018
+ */
+package ru.job4j.generics.store;
