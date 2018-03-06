8f21252c671c5c7a4d9c206f539af0dd39094640
vermucht <vermucht@gmail.com>
2018-03-06 10:17:02 +0300

    2. Set реализованный на связном списке. [#997]

---
+++ 002_junior_chapter_001/src/main/java/ru/job4j/set/onlist/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Simple set based on object implementing SimpleContainer interface.
+ * Has two realizations: on SimpleArrayList and on SimpleLinkedList.
+ * Allows to store, add and get unique elements.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 05.03.2018
+ */
+package ru.job4j.set.onlist;
