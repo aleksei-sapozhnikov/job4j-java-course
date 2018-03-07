b513e0ba91492807bafbfa7a3ec0e06fe2ba2d76
vermucht <vermucht@gmail.com>
2018-03-07 19:28:09 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008]

---
+++ 002_junior_chapter_001/src/main/java/ru/job4j/map/equalsandhash/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Simple tests what happens in Map when adding
+ * objects of User Class with overridden equals()
+ * and hashCode() methods or not.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 07.03.2018
+ */
+package ru.job4j.map.equalsandhash;
---
+++ 002_junior_chapter_001/src/main/java/ru/job4j/map/hashmap/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Simple hash map realisation.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 07.03.2018
+ */
+package ru.job4j.map.hashmap;
+
+
