9b13e6318de3717e5a1f15485e22d3bbd870c8c7
vermucht <vermucht@gmail.com>
2018-03-27 13:29:24 +0300

    1. Cчитать количество слов и пробелов в тексте [#1016]

---
+++ 002_junior_chapter_002/src/main/java/ru/job4j/count/CountWordsAndSpaces.java
@@ -0,0 +1,71 @@
+package ru.job4j.count;
+
+/**
+ * Counts words and spaces in given input.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 27.03.2018
+ */
+class CountWordsAndSpaces {
+    /**
+     * Input string.
+     */
+    private String input;
+
+    /**
+     * Constructs new object with given input string.
+     *
+     * @param input input string.
+     */
+    CountWordsAndSpaces(String input) {
+        this.input = input;
+    }
+
+    /**
+     * Starts multi-threaded operations counting words and spaces.
+     */
+    void start() {
+        Thread space = new Thread(() -> System.out.printf("=== Words : %s%n", this.countWords()));
+        Thread word = new Thread(() -> System.out.printf("=== Spaces : %s%n", this.countSpaces()));
+        word.start();
+        space.start();
+    }
+
+    /**
+     * Counts number of spaces in the input string.
+     *
+     * @return number of spaces.
+     */
+    int countSpaces() {
+        char[] chars = this.input.toCharArray();
+        int count = 0;
+        for (char c : chars) {
+            if (c == ' ') {
+                count++;
+                System.out.println(String.format("spaces count : %s", count));
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Counts number of words in given string.
+     *
+     * @return number of words.
+     */
+    int countWords() {
+        char[] chars = this.input.toCharArray();
+        int count = 0;
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == ' ' && chars[i - 1] != ' ') {
+                count++;
+                System.out.println(String.format("words count : %s", count));
+            }
+        }
+        if (chars[chars.length - 1] != ' ') {       // last word
+            count++;
+        }
+        return count;
+    }
+}
