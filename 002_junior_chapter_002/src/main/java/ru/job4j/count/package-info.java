198d2e6cd8cc804d8f632f5a63e078090e063d53
vermucht <vermucht@gmail.com>
2018-03-30 16:09:16 +0300

    package-info

---
+++ 002_junior_chapter_002/src/main/java/ru/job4j/count/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Simple multi-thread count of words and spaces separately.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 27.03.2018
+ */
+package ru.job4j.count;
---
+++ 002_junior_chapter_002/src/main/java/ru/job4j/interrupt/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Thread of Time class launches thread of CountChars class and interrupts it if working time is too long.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 28.03.2018
+ */
+package ru.job4j.interrupt;
