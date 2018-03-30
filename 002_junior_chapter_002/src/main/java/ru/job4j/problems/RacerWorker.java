5ed60414f141c807e83d95fdf93db42112c48e75
vermucht <vermucht@gmail.com>
2018-03-30 17:48:29 +0300

    1. Проиллюстрировать проблемы с многопоточностью. [#1096]

--- 002_junior_chapter_002/src/main/java/ru/job4j/problems/RacerWorker.java
+++ 002_junior_chapter_002/src/main/java/ru/job4j/problems/RacerWorker.java
@@ -1,19 +1,43 @@
 package ru.job4j.problems;
 
+/**
+ * Thread giving information about professions with some "sleeps" during work.
+ * To illustrate problems of race condition.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version $Id$
+ * @since 30.03.2018
+ */
 public class RacerWorker implements Runnable {
-
+    /**
+     * Number of the profession and duty in given arrays.
+     */
     private int number;
-
+    /**
+     * Array of professions.
+     */
     private String[] professions;
-
+    /**
+     * Array of duties.
+     */
     private String[] duties;
 
+    /**
+     * Constructs a new worker.
+     *
+     * @param number      number of the profession and duty.
+     * @param professions list of professions.
+     * @param duties      list of duties.
+     */
     RacerWorker(int number, String[] professions, String[] duties) {
         this.number = number;
         this.professions = professions;
         this.duties = duties;
     }
 
+    /**
+     * Runs when called by Thread.start()
+     */
     public void run() {
         try {
             System.out.printf("Профессия №%s: ", this.number);
@@ -25,4 +49,5 @@ public class RacerWorker implements Runnable {
             ie.printStackTrace();
         }
     }
+
 }
