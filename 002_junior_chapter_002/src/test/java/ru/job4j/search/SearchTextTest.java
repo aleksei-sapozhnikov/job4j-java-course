c110aaeac5d81fa018b177010ddff4c1cd306df8
vermucht <vermucht@gmail.com>
2018-04-14 18:15:42 +0300

    4. Поиск текста в файле. [#1106]

--- 002_junior_chapter_002/src/test/java/ru/job4j/search/SearchTextTest.java
+++ 002_junior_chapter_002/src/test/java/ru/job4j/search/SearchTextTest.java
@@ -1,14 +1,54 @@
 package ru.job4j.search;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class SearchTextTest {
+
+    @Rule
+    private TemporaryFolder root = new TemporaryFolder();
+
+
+    @Before
+    public void createFoldersAndFiles() throws IOException {
+
+
+    }
+
+    private void createFoldersPaths() throws IOException {
+
+    }
+
+    private void createFiles() throws IOException {
+        // root folders
+        Path k1 = root.newFolder("K1").toPath();
+
+
+        List<Path> paths = new ArrayList<>(Arrays.asList(
+                // root files
+                root.newFile("File1.aaa").toPath(),
+                root.newFile("File2.bbb").toPath(),
+                root.newFile("File3.xxx").toPath(),
+                // root folders
+                root.newFolder("K1%sSK1").toPath()
+        ));
+    }
+
+    @Test
+    public void testUsingTempFolder() throws IOException {
+        // ...
+    }
+
 
     @Test
     public void performSearch() {
