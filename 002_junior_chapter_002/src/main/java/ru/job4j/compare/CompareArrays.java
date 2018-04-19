a26409b438424d43cba11d00b77375f296899c26
vermucht <vermucht@gmail.com>
2018-04-19 17:57:56 +0300

    Сравнение массивов

--- 002_junior_chapter_002/src/main/java/ru/job4j/compare/CompareArrays.java
+++ 002_junior_chapter_002/src/main/java/ru/job4j/compare/CompareArrays.java
@@ -1,9 +1,8 @@
 package ru.job4j.compare;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-class CompareArrays<T> {
+class CompareArrays<T extends Comparable<T>> {
     private T[] left;
     private T[] right;
 
@@ -12,16 +11,54 @@ class CompareArrays<T> {
         this.right = right;
     }
 
-    boolean byMap() {
-        Map<T, Integer> mLeft = new HashMap<>();
-        Map<T, Integer> mRight = new HashMap<>();
-        for (T element : left) {
-            Integer existing = mLeft.computeIfPresent(element, (k, v) -> (v + 1));
+    boolean byHashMap() {
+        Map<T, Integer> mLeft = this.arrayToMap(this.left, new HashMap<>());
+        Map<T, Integer> mRight = this.arrayToMap(this.right, new HashMap<>());
+        return mLeft.equals(mRight);
+    }
+
+    boolean byTreeMap() {
+        Map<T, Integer> mLeft = this.arrayToMap(this.left, new TreeMap<>());
+        Map<T, Integer> mRight = this.arrayToMap(this.right, new TreeMap<>());
+        return mLeft.equals(mRight);
+    }
+
+    private Map<T, Integer> arrayToMap(T[] array, Map<T, Integer> map) {
+        for (T element : array) {
+            Integer existing = map.computeIfPresent(element, (k, v) -> (v + 1));
             if (existing == null) {
-                mLeft.put(element, 1);
+                map.put(element, 1);
             }
         }
-        return false;
+        return map;
+    }
+
+    boolean byList() {
+        List<T> lLeft = new ArrayList<>(Arrays.asList(this.left));
+        List<T> lRight = new ArrayList<>(Arrays.asList(this.right));
+        Collections.sort(lLeft);
+        Collections.sort(lRight);
+        return lLeft.equals(lRight);
+    }
+
+    boolean byListOneByOne() {
+        boolean result = true;
+        List<T> lLeft = new ArrayList<>(Arrays.asList(this.left));
+        List<T> lRight = new ArrayList<>(Arrays.asList(this.right));
+        for (T left : lLeft) {
+            boolean has = false;
+            for (T right : lRight) {
+                if (left.compareTo(right) == 0) {
+                    has = true;
+                    break;
+                }
+            }
+            if (!has) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 
 }
