de17b2bc9a0cf893497bb1249d35046914d276fe
vermucht <vermucht@gmail.com>
2018-04-26 16:27:29 +0300

    Игра бомбермен. [#1108] - графика 37:35

--- 002_junior_chapter_002/src/test/java/ru/job4j/bomberman/GameTest.java
+++ 002_junior_chapter_002/src/test/java/ru/job4j/bomberman/GameTest.java
@@ -6,8 +6,8 @@ public class GameTest {
 
     @Test
     public void runSomethingPlease() throws InterruptedException {
-        Game game = new Game();
         Board board = new Board(3, 3);
+        Game game = new Game();
         game.add(new Character(board, 1, "Player", 0, 0));
         game.add(new Character(board, 2, "Monster_1", 2, 0));
         game.add(new Character(board, 3, "Monster_2", 0, 0));
