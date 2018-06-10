d7cd15ab263c36355b29fb121541ba1bfac4084a
vermucht <vermucht@gmail.com>
2018-06-10 18:07:21 +0300

    Парсер вакансий на sql.ru [#1731] 00:32:18

--- 002_junior_chapter_003/src/main/java/ru/job4j/vacancies/PageParser.java
+++ 002_junior_chapter_003/src/main/java/ru/job4j/vacancies/PageParser.java
@@ -9,10 +9,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.ParseException;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.LinkedList;
@@ -24,22 +21,28 @@ import java.util.Locale;
  */
 public class PageParser {
     public static final String TODAY = "сегодня";
+
     public static final String YESTERDAY = "вчера";
+
+    public static final DateTimeFormatter DATE_TIME_STRING_FORMAT = DateTimeFormatter.ofPattern(
+            "d MMM uu, kk:mm"
+    ).withLocale(Locale.forLanguageTag("ru"));
+
+    public static final DateTimeFormatter DATE_FORMAT_TODAY_YESTERDAY = DateTimeFormatter.ofPattern(
+            "d MMM uu"
+    ).withLocale(Locale.forLanguageTag("ru"));
 
     public static void main(String[] args) throws ParseException, IOException {
         PageParser parser = new PageParser();
 
-        List<Vacancy> found = parser.parseFile(Paths.get(
-                "002_junior_chapter_003/src/main/resources/ru/job4j/vacancies/test_sources/test_parser_sample_1.htm"
-        ));
+        //List<Vacancy> found = parser.parseUrl("http://www.sql.ru/forum/job-offers");
 
-        List<Vacancy> found1 = parser.parseUrl("http://www.sql.ru/forum/job-offers");
-        for (int i = 0; i < found.size(); i++) {
-            System.out.println("F I L E: ");
-            System.out.println(found.get(i));
-            System.out.println("U R L: ");
-            System.out.println(found1.get(i));
-            System.out.println("===============================");
+        List<Vacancy> found = parser.parseFile(
+                Paths.get("002_junior_chapter_003/src/main/resources/ru/job4j/vacancies/test_sources/test_parser_sample_1.htm")
+        );
+
+        for (Vacancy f : found) {
+            System.out.println(f);
         }
     }
 
@@ -58,68 +61,48 @@ public class PageParser {
         List<Vacancy> result = new LinkedList<>();
         Elements rows = document.select("tr");
         for (Element row : rows) {
-            Element themeEl = row.selectFirst("td.postslisttopic a[href]");
-            Element dateEl = row.selectFirst("td.altCol[style='text-align:center']");
-            if (validateThemeAndDate(themeEl, dateEl)) {
+            Element theme = row.selectFirst("td.postslisttopic a[href]");
+            Element published = row.selectFirst("td.altCol[style='text-align:center']");
+            if (isValid(theme, published)) {
                 result.add(new Vacancy(
-                        themeEl.text(),
-                        themeEl.attr("abs:href"),
-                        this.parseDateStringDispatch(dateEl.text())
-                ));
+                        theme.text(), theme.attr("abs:href"),
+                        this.stringToDate(published.text()))
+                );
             }
         }
         return result;
     }
 
-    private LocalDateTime parseDateStringDispatch(String date) throws ParseException {
+    private LocalDateTime stringToDate(String date) throws ParseException {
         LocalDateTime result;
         if (date.startsWith(TODAY)) {
-            result = this.parseDateStringTodayYesterday(date, true);
+            result = this.parseDateString(
+                    this.todayYesterdayToDate(date, true));
         } else if (date.startsWith(YESTERDAY)) {
-            result = this.parseDateStringTodayYesterday(date, false);
+            result = this.parseDateString(
+                    this.todayYesterdayToDate(date, false));
         } else {
-            result = this.parseDateStringOtherDay(date);
+            result = this.parseDateString(date);
         }
         return result;
     }
 
-    private LocalDateTime parseDateStringOtherDay(String date) {
-        DateTimeFormatter format = DateTimeFormatter.ofPattern(
-                "d MMM uu, kk:mm"
-        ).withLocale(Locale.forLanguageTag("ru"));
-        return LocalDateTime.parse(date, format);
+    private LocalDateTime parseDateString(String date) {
+        return LocalDateTime.parse(date, DATE_TIME_STRING_FORMAT);
     }
 
-    private LocalDateTime parseDateStringTodayYesterday(String date, boolean todayYesterday)
-            throws ParseException {
-        LocalDateTime result;
-        try {
-            int startHour = date.length() - 5;
-            int startMinute = date.length() - 2;
-            int endHour = startHour + 2;
-            int endMinute = startMinute + 2;
-            int hour = Integer.valueOf(date.substring(startHour, endHour));
-            int minute = Integer.valueOf(date.substring(startMinute, endMinute));
-            result = this.getTimeAsObjectTodayYesterday(todayYesterday, hour, minute);
-        } catch (NumberFormatException e) {
-            throw new ParseException("Could not get integer hour or minute value from string", 0);
-        }
-        return result;
+    private String todayYesterdayToDate(String string, boolean todayYesterday) {
+        Instant day = todayYesterday
+                ? Instant.now()
+                : Instant.now().minus(1, ChronoUnit.DAYS);
+        LocalDate date = ZonedDateTime.ofInstant(day, ZoneId.of("Europe/Moscow")).toLocalDate();
+        String time = string.substring(todayYesterday
+                ? TODAY.length()
+                : YESTERDAY.length());
+        return String.format("%s%s", date.format(DATE_FORMAT_TODAY_YESTERDAY), time);
     }
 
-    private LocalDateTime getTimeAsObjectTodayYesterday(boolean todayYesterday, int hour, int minute) {
-        long fromMidnight = hour * 60 + minute;
-        ZonedDateTime getDay = ZonedDateTime.ofInstant(
-                todayYesterday
-                        ? Instant.now()
-                        : Instant.now().minus(1, ChronoUnit.DAYS),
-                ZoneId.of("Europe/Moscow")
-        );
-        LocalDateTime startDay = getDay.toLocalDate().atStartOfDay();
-        return startDay.plus(fromMidnight, ChronoUnit.MINUTES);
-    }
-
-    private boolean validateThemeAndDate(Element theme, Element date) {
+    private boolean isValid(Element theme, Element date) {
         boolean notNull = theme != null && date != null;
         String lower = notNull ? theme.text().toLowerCase() : "";
         return notNull
