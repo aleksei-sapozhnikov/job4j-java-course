d7413ffb2db3e1553ce5e8b559145f662adf44bf
vermucht <vermucht@gmail.com>
2018-08-14 17:15:50 +0300

    Музыкальная прощадка [#3236] 45:24 Begin making DAO and DAO factory.

--- 002_junior_chapter_004/music_store/src/main/java/ru/job4j/music/UserDao.java
+++ 002_junior_chapter_004/music_store/src/main/java/ru/job4j/music/UserDao.java
@@ -6,8 +6,12 @@ import org.apache.logging.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Map;
+
+import static ru.job4j.music.DaoFactory.DaoOperations.ADD;
+import static ru.job4j.music.DaoFactory.DaoOperations.GET_BY_ID;
 
 /**
  * TODO: class description
@@ -29,11 +33,32 @@ public class UserDao extends GenericDao<User> {
     @Override
     public User add(User entity) throws SQLException {
         try (Connection connection = this.connectionPool.getConnection();
-             PreparedStatement add = connection.prepareStatement(this.queries.get(DaoFactory.DaoOperations.ADD));
-             PreparedStatement get = connection.prepareStatement(this.queries.get(DaoFactory.DaoOperations.GET_BY_ID))
+             PreparedStatement add = connection.prepareStatement(this.queries.get(ADD));
+             PreparedStatement get = connection.prepareStatement(this.queries.get(GET_BY_ID));
         ) {
+            add.executeQuery();
+            get.executeQuery();
         } catch (SQLException e) {
             LOG.error(CommonMethods.describeException(e));
         }
+        return null;
     }
+
+    private int addUser(User user) {
+        try (Connection connection = this.connectionPool.getConnection();
+             PreparedStatement userAdd = connection.prepareStatement(this.queries.get(ADD));
+
+        ) {
+            // INSERT INTO users (login, password, role_id, address_id) VALUES (?, ?, ?, ?) RETURNING id;
+            userAdd.setString(1, user.getLogin());
+            userAdd.setString(2, user.getPassword());
+            try (ResultSet res = userAdd.executeQuery()) {
+
+            }
+        } catch (SQLException e) {
+            LOG.error(CommonMethods.describeException(e));
+        }
+        return 0;
+    }
+
 }
