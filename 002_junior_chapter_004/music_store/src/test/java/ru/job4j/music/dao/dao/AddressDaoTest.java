9b82c9b566ca746d7a7d71c5e8019f277e5a501f
vermucht <vermucht@gmail.com>
2018-08-22 08:32:22 +0300

    Музыкальная прощадка [#3236] 59:49 Made Address dao and tests.

--- 002_junior_chapter_004/music_store/src/test/java/ru/job4j/music/dao/dao/AddressDaoTest.java
+++ 002_junior_chapter_004/music_store/src/test/java/ru/job4j/music/dao/dao/AddressDaoTest.java
@@ -3,7 +3,6 @@ package ru.job4j.music.dao.dao;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.junit.Before;
 import org.junit.Test;
-import ru.job4j.music.dao.general.Dao;
 import ru.job4j.music.dao.general.DaoPool;
 import ru.job4j.music.dao.general.DbConnector;
 import ru.job4j.music.dao.general.DbStructureChanger;
@@ -11,7 +10,12 @@ import ru.job4j.music.entities.Address;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class AddressDaoTest {
 
@@ -33,32 +37,51 @@ public class AddressDaoTest {
         this.structure.clear();
     }
 
+    /**
+     * Test add() and get()
+     */
     @Test
-    public void add() {
+    public void whenAddThenReturnedAdded() {
         Address added = this.dao.add(this.addressWithId);
-        Address added2 = this.dao.add(this.addressWithoutId);
         Address found = this.dao.get(added.getId());
-        Address found2 = this.dao.get(added2.getId());
         List<Address> list = this.dao.getAll();
-        System.out.println(found);
-        System.out.println(found2);
-        System.out.println();
-        System.out.println(list);
+        assertThat(added, is(found));
+        assertThat(found, is(this.addressWithId));
+        assertThat(list, is(Collections.singletonList(this.addressWithId)));
     }
 
+    /**
+     * Test update()
+     */
     @Test
-    public void get() {
+    public void whenUpdateThenNewValuesAndUpdaterIdDoesNotMatter() {
+        Address added = this.dao.add(this.addressWithoutId);
+        Address result = this.dao.update(added.getId(), this.addressWithId);
+        assertThat(result, is(addressWithId));
+        assertThat(result.getId(), is(added.getId()));
     }
 
+    /**
+     * Test delete()
+     */
     @Test
-    public void update() {
+    public void whenDeleteThenReturnDeletedAndNoMoreInDatabase() {
+        Address added = this.dao.add(this.addressWithId);
+        Address deleted = this.dao.delete(added.getId());
+        assertThat(added, is(deleted));
+        assertThat(this.dao.get(added.getId()), is(Address.EMPTY_ADDRESS));
+        assertThat(this.dao.getAll(), is(Collections.emptyList()));
     }
 
+    /**
+     * Test getAll()
+     */
     @Test
-    public void delete() {
-    }
-
-    @Test
-    public void getAll() {
+    public void whenGetAllThenListOfAddressesInDatabase() {
+        this.dao.add(this.addressWithId);
+        this.dao.add(this.addressWithoutId);
+        List<Address> result = this.dao.getAll();
+        List<Address> expected = Arrays.asList(this.addressWithId, this.addressWithoutId);
+        assertThat(result, is(expected));
     }
 }
