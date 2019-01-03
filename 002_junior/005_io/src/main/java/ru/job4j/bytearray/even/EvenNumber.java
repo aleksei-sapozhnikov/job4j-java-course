b35c693bbd79cc006bea26c4ec6f3b99ab4c655a
vermucht <bK36!N#Zko6bfF%F5A@R>
2019-01-03 21:27:14 +0800

    1. Проверить байтовый поток [#858]

---
+++ 002_junior/005_io/src/main/java/ru/job4j/bytearray/even/EvenNumber.java
@@ -0,0 +1,41 @@
+package ru.job4j.bytearray.even;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+/**
+ * Finds if byte array is even number.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version 0.1
+ * @since 0.1
+ */
+public class EvenNumber {
+
+    /**
+     * Returns if given Input stream is an even number.
+     * First way: using only regexp.
+     *
+     * @param in InputStream object.
+     * @return <tt>true</tt> if even number, <tt>false</tt> if not.
+     * @throws IOException In case of problems with reading bytes from InputStream.
+     */
+    public boolean isEvenNumberRegexp(InputStream in) throws IOException {
+        return new String(in.readAllBytes()).matches("^\\d*[02468]$");
+    }
+
+    /**
+     * Returns if given Input stream is an even number.
+     * Second way: using regexp for validate, then parsing to long.
+     *
+     * @param in InputStream object.
+     * @return <tt>true</tt> if even number, <tt>false</tt> if not.
+     * @throws IOException In case of problems with reading bytes from InputStream.
+     */
+    public boolean isEvenNumberLong(InputStream in) throws IOException {
+        var s = new String(in.readAllBytes());
+        return s.matches("^\\d+")
+                && Long.parseLong(s) % 2 == 0;
+    }
+}
---
+++ 002_junior/005_io/src/test/java/ru/job4j/bytearray/even/IsEvenNumberTest.java
@@ -0,0 +1,68 @@
+package ru.job4j.bytearray.even;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class IsEvenNumberTest {
+    private final EvenNumber test = new EvenNumber();
+
+    /**
+     * Test isEvenNumberRegexp()
+     */
+    @Test
+    public void whenEvenNumberThenTrueMethodRegexp() throws IOException {
+        String number = "12";
+        try (InputStream in = new ByteArrayInputStream(number.getBytes())) {
+            assertThat(this.test.isEvenNumberRegexp(in), is(true));
+        }
+    }
+
+    @Test
+    public void whenOddNumberThenFalseMethodRegexp() throws IOException {
+        String number = "11";
+        try (InputStream in = new ByteArrayInputStream(number.getBytes())) {
+            assertThat(this.test.isEvenNumberRegexp(in), is(false));
+        }
+    }
+
+    @Test
+    public void whenNotNumberThenFalseMethodRegexp() throws IOException {
+        String number = "four12";
+        try (InputStream in = new ByteArrayInputStream(number.getBytes())) {
+            assertThat(this.test.isEvenNumberRegexp(in), is(false));
+        }
+    }
+
+    /**
+     * Test isEvenNumberLong()
+     */
+    @Test
+    public void whenEvenNumberThenTrueMethodLong() throws IOException {
+        String number = "12";
+        try (InputStream in = new ByteArrayInputStream(number.getBytes())) {
+            assertThat(this.test.isEvenNumberLong(in), is(true));
+        }
+    }
+
+    @Test
+    public void whenOddNumberThenFalseMethodLong() throws IOException {
+        String number = "11";
+        try (InputStream in = new ByteArrayInputStream(number.getBytes())) {
+            assertThat(this.test.isEvenNumberLong(in), is(false));
+        }
+    }
+
+    @Test
+    public void whenNotNumberThenFalseMethodLong() throws IOException {
+        String number = "four12";
+        try (InputStream in = new ByteArrayInputStream(number.getBytes())) {
+            assertThat(this.test.isEvenNumberLong(in), is(false));
+        }
+    }
+}
